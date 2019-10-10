0fd1cecab6dd2b58ca4ba1aad7487f942a86ef4b
Steve Clarke <sclarke500@gmail.com>
2019-10-10 12:35:32 -0400

    lose subfolder, hook db back up

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ app.get('/', (req, res) => res.send('works!'));
 
 // connect to mongodb
 mongoose.set('useCreateIndex', true); //fixes an issue with a depricated default in Mongoose.js
-// mongoose
-//   .connect('mongodb+srv://testapp:genesis@cluster0-d3bja.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => console.log('Connected to mongo...'))
-//   .catch(err => console.error('Could not connect to Mongo'));
+mongoose
+  .connect('mongodb+srv://testapp:genesis@cluster0-d3bja.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to mongo...'))
+  .catch(err => console.error('Could not connect to Mongo'));
 
 
 app.listen(port, () => console.log(`listening on port ${port}`));

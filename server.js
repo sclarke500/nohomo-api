6d0d46145417d40682669826757808c119714e05
Steve Clarke <sclarke500@gmail.com>
2019-10-10 20:48:10 -0400

    adding cors

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 const port = (process.env.PORT || 3000);
+app.use(cors());
 app.use(express.json());
 
 const userRoute = require('./routes/users.route');

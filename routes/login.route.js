b78f4654784717cef9f2062cfd742b1d3ebfc213
sclarke500 <sclarke500@gmail.com>
2020-02-26 18:24:51 -0500

    password reset doc delete

--- routes/login.route.js
+++ routes/login.route.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const express = require('express');
 const router = express.Router();
 const { User } = require('../models/user.model');
@@ -8,9 +9,9 @@ const auth = require('../middleware/auth');
 
 router.post('/login', async (req, res) => {
 
-  let user = await User.findOne( {email: req.body.email });
+  let user = await User.findOne({ email: req.body.email });
   if (user) {
-    let firm = await Firm.findOne( { _id: user.firm_id });
+    let firm = await Firm.findOne({ _id: user.firm_id });
     bcrypt.compare(req.body.password, user.password, (err, bcRes) => {
       if (bcRes) {
         // login success!
@@ -25,17 +26,66 @@ router.post('/login', async (req, res) => {
         }
         res.json(loginModel);
       } else {
-        res.status(400).json({message: 'Bad credentials 2'});
+        res.status(400).json({ message: 'Bad credentials 2' });
       }
     })
   } else {
-    res.status(400).json({message: 'Bad credentials'});
+    res.status(400).json({ message: 'Bad credentials' });
   }
 
 });
 
 router.post('/forgot', async (req, res) => {
-  res.json({message: 'cool'});
+
+  // lookup email
+  const _email = req.body.email;
+  let user = await User.findOne({ email:  _email });
+  if (user) {
+    
+    const token = jwt.sign({ email:  _email }, process.env.SECRET_KEY, { expiresIn: '24h' });
+    const emailLinkUrl = req.headers.origin + '/reset-password?token=' + token;
+
+    const sgMail = require('@sendgrid/mail');
+    sgMail.setApiKey(process.env.SG_KEY);
+    const msg = {
+      to: user.email,
+      from: 'sclarke250@gmail.com',
+      subject: 'Password Reset Link',
+      text: 'and easy to do anywhere, even with Node.js',
+      html: 'Reset Link:<br><br><br><a href="' + emailLinkUrl + '" target="_blank">Reset</a>',
+    };
+    
+    sgMail.send(msg);
+    res.json({ message: 'ok', body: 'Reset Link:<br><br><br><a href="' + emailLinkUrl + '" target="_blank">Reset</a>' });
+  } else {
+    res.status(404).json({ message: 'not found' });
+  }
+
+
+
+
+});
+
+router.post('/reset-password', async (req, res) => {
+
+  // look up user, validate token, set password
+  const token = req.body.token;
+  if (!token) return res.status(401).send('Access denied. NT');
+
+  try {
+    const tModel = jwt.verify(token, process.env.SECRET_KEY);
+    let user = await User.findOne({ email:  tModel.email });
+    if (user) {
+      user.password = await bcrypt.hash(req.body.password, 10);
+      await user.save();
+      res.json({status: 'ok'});
+    } else {
+      res.status(404).send();
+    }
+  } catch (e) {
+    res.status(400).send();
+  }
+
 });
 
 module.exports = router;
--- routes/matters.route.js
+++ routes/matters.route.js
@@ -8,38 +8,38 @@ const { Matter } = require('../models/matter.model');
 const { Document } = require('../models/document.model');
 
 router.get('/', async (req, res) => {
- const matters = await Matter.find({ firm_id: req.user.firm_id });
- res.json(matters);
+  const matters = await Matter.find({ firm_id: req.user.firm_id });
+  res.json(matters);
 });
 
 router.get('/:matterId', async (req, res) => {
   const matterId = req.params.matterId;
   let matter = await Matter.findOne({ firm_id: req.user.firm_id, _id: matterId });
-  if (!matter) { res.status(401).json({message: 'unauthorized'})} // move to middleware?
+  if (!matter) { res.status(401).json({ message: 'unauthorized' }) } // move to middleware?
   res.json(matter);
- });
- 
- router.get('/:matterId/documents', async (req, res) => {
+});
+
+router.get('/:matterId/documents', async (req, res) => {
   const matterId = req.params.matterId;
   let matter = await Matter.findOne({ firm_id: req.user.firm_id, _id: matterId });
-  if (!matter) { res.status(401).json({message: 'unauthorized'})} // move to middleware?
-  const docs = await Document.find( { matter_id: matterId });
+  if (!matter) { res.status(401).json({ message: 'unauthorized' }) } // move to middleware?
+  const docs = await Document.find({ matter_id: matterId });
   res.json(docs);
- });
+});
 
 
- router.post('/:matterId/documents', async (req, res) => {
+router.post('/:matterId/documents', async (req, res) => {
   const matterId = req.params.matterId;
   const matter = await Matter.findOne({ firm_id: req.user.firm_id, _id: matterId }); // ensure we own
-  if (!matter) { res.status(401).json({message: 'unauthorized'})} // move to middleware?
-  const doc = new Document({name: req.body.name, matter_id: matterId});
+  if (!matter) { res.status(401).json({ message: 'unauthorized' }) } // move to middleware?
+  const doc = new Document({ name: req.body.name, matter_id: matterId });
   const d = await doc.save();
   res.json(d);
 });
 
 router.post('/', async (req, res) => {
-  const m = { 
-    name: req.body.name, 
+  const m = {
+    name: req.body.name,
     firm_id: req.user.firm_id,
   };
   const matter = new Matter(m);
@@ -50,10 +50,22 @@ router.post('/', async (req, res) => {
 router.delete('/:matterId', async (req, res) => {
   const matterId = req.params.matterId;
   const matter = await Matter.findOne({ firm_id: req.user.firm_id, _id: matterId }); // ensure we own
-  if (!matter) { res.status(401).json({message: 'unauthorized'})} // move to middleware?
+  if (!matter) { res.status(401).json({ message: 'unauthorized' }); return; } // move to middleware?
   await Matter.deleteOne({ _id: matterId })
   res.send();
- });
+});
+
+router.delete('/:matterId/documents/:documentId', async (req, res) => {
+  const matterId = req.params.matterId;
+  const documentId = req.params.documentId;
+  const matter = await Matter.findOne({ firm_id: req.user.firm_id, _id: matterId }); // ensure we own
+  const document = await Document.findOne({ matter_id: matterId, _id: documentId }); // ensure matter owns
+  console.log(matterId, documentId, matter, document)
+  if (!matter || !document) { res.status(401).json({ message: 'unauthorized' }); return; } // move to middleware?
+  await Document.deleteOne({ _id: documentId })
+  res.send();
+});
+
 
 
 module.exports = router;
